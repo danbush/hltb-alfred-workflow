15bdd978c9aeff25e0c213ab0ec341ded7e0cc88
Dan Bush <dan@knows.email>
2022-09-14 17:10:19 -0500

    initial pass at updating the code, not yet working

--- hltb.rb
+++ hltb.rb
@@ -11,6 +11,8 @@ require 'alfred-3_workflow'
 require 'open-uri'
 require 'fileutils'
 
+require 'json'
+
 workflow = Alfred3::Workflow.new
 
 query = ARGV[0]
@@ -35,29 +37,34 @@ image_path_1 = ""
 image_path_2 = ""
 image_path_3 = ""
 
-params = { :page => "1", :queryString => query, :t => "games", :sorthead => "popular", :sortd => "Normal%20Order", :plat => "", :length_type => "main", :length_min => "", :length_max => "", :detail => "" }
+parsed_response = ""
 
 response =
-  HTTParty.post("https://howlongtobeat.com/search_results?page=1",
+  HTTParty.post("https://howlongtobeat.com/api/search",
   :headers => {
+      'Content-Type' => "application/json",
       "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0",
       "Referer" => "https://howlongtobeat.com/"
   },    
-  :body => params
+  :body => {"searchType":"games","searchTerms":["zelda"],"searchPage":1,"size":20,"searchOptions":{"games":{"userId":0,"platform":"","sortCategory":"popular","rangeCategory":"main","rangeTime":{"min":0,"max":0},"gameplay":{"perspective":"","flow":"","genre":""},"modifier":""},"users":{"sortCategory":"postcount"},"filter":"","sort":0,"randomizer":0}}.to_json
   )
+  
+game_data = response["data"]
 
-game_id = (response.scan(/id=(\d{4,5})/))
+game_id = game_data.map { |h| h['game_id'] }.uniq
 
-game_names = response.scan(/aria-label="([^"]*)"/)
+game_names = game_data.map { |h| h['game_name'] }
 
-game_difficulty_labels = response.scan(/<div class="search_list_tidbit text_white shadow_text">([^"]*)<\/div>/)
+game_difficulty_labels = ["Main", "Main + Extra", "Completionist"]
 
-game_difficulties = response.scan(/<div class="search_list_tidbit center time_\d+">([^"]*)<\/div>/)
+game_difficulties_main = game_data.map { |h| h['comp_main'] }
+game_difficulties_extra = game_data.map { |h| h['comp_plus'] }
+game_difficulties_complete = game_data.map { |h| h['comp_100'] }
 
-game_image = response.scan(/alt="Box Art" src="([^"]*)/)
+game_image = game_data.map { |h| h['game_image'] }
 
 if game_image[0] != nil
-  game_image_1 = 'https://howlongtobeat.com' + game_image[0].join('')
+  game_image_1 = 'https://howlongtobeat.com/games/' + game_image[0]
   image_path_1 = "./images/" + game_names[0].to_s.downcase.sub('aria-label="', '').sub('["','').sub('"]','').sub(' ', '_') + ".jpg"
   open(image_path_1, 'wb') do |file|
     file << open(game_image_1).read
@@ -65,14 +72,14 @@ if game_image[0] != nil
 end
 
 if game_image[1] != nil
-  game_image_2 = 'https://howlongtobeat.com' + game_image[1].join('')
+  game_image_2 = 'https://howlongtobeat.com/games/' + game_image[1]
   image_path_2 = "./images/" + game_names[1].to_s.downcase.sub('aria-label="', '').sub('["','').sub('"]','').sub(' ', '_') + ".jpg"
   open(image_path_2, 'wb') do |file|
     file << open(game_image_2).read
   end
 end
 if game_image[2] != nil
-  game_image_3 = 'https://howlongtobeat.com' + game_image[2].join('')
+  game_image_3 = 'https://howlongtobeat.com/games/' + game_image[2]
   image_path_3 = "./images/" + game_names[2].to_s.downcase.sub('aria-label="', '').sub('["','').sub('"]','').sub(' ', '_') + ".jpg"
   open(image_path_3, 'wb') do |file|
     file << open(game_image_3).read
@@ -80,21 +87,21 @@ if game_image[2] != nil
 end
 
 if game_image[0] != nil
-  difficulty_1 = game_difficulties[0].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_2 = game_difficulties[1].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_3 = game_difficulties[2].join('').sub("&#189;", ".5").sub("</div>\n</div> </div>\n", "").sub("</div>", "")
+  difficulty_1 = game_difficulties[0]
+  difficulty_2 = game_difficulties[1]
+  difficulty_3 = game_difficulties[2]
 end
 
 if game_image[1] != nil
-  difficulty_4 = game_difficulties[3].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_5 = game_difficulties[4].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_6 = game_difficulties[5].join('').sub("&#189;", ".5").sub("</div>\n</div> </div>\n", "").sub("</div>", "")
+  difficulty_4 = game_difficulties[3]
+  difficulty_5 = game_difficulties[4]
+  difficulty_6 = game_difficulties[5]
 end
 
 if game_image[2] != nil
-  difficulty_7 = game_difficulties[6].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_8 = game_difficulties[7].join('').sub("&#189;", ".5").sub("</div>", "")
-  difficulty_9 = game_difficulties[8].join('').sub("&#189;", ".5").sub("</div>\n</div> </div>\n", "").sub("</div>", "")
+  difficulty_7 = game_difficulties[6]
+  difficulty_8 = game_difficulties[7]
+  difficulty_9 = game_difficulties[8]
 end
 
 if query == "update!"
@@ -104,24 +111,24 @@ if query == "update!"
       .arg("update!")
 elsif game_names[0] != nil
   workflow.result
-        .title(game_names[0].join(''))
-        .subtitle(game_difficulty_labels[0].join('') + " - " + difficulty_1 + " | " + game_difficulty_labels[1].join('') + " - " + difficulty_2 + " | " + game_difficulty_labels[2].join('') + " - " + difficulty_3)
+        .title(game_names[0])
+        .subtitle(game_difficulty_labels[0] + " - " + difficulty_1 + " | " + game_difficulty_labels[1] + " - " + difficulty_2 + " | " + game_difficulty_labels[2] + " - " + difficulty_3)
         .icon(image_path_1)
-        .arg(game_id[0].join(''))
+        .arg(game_id[0])
   if difficulty_4 != nil
     workflow.result
-          .title(game_names[1].join(''))
-          .subtitle(game_difficulty_labels[0].join('') + " - " + difficulty_4 + " | " + game_difficulty_labels[1].join('') + " - " + difficulty_5 + " | " + game_difficulty_labels[2].join('') + " - " + difficulty_6)
+          .title(game_names[1])
+          .subtitle(game_difficulty_labels[0] + " - " + difficulty_4 + " | " + game_difficulty_labels[1] + " - " + difficulty_5 + " | " + game_difficulty_labels[2] + " - " + difficulty_6)
           .icon(image_path_2)
-          .arg(game_id[1].join(''))
+          .arg(game_id[1])
   end
 
   if difficulty_7 != nil
     workflow.result
-          .title(game_names[2].join(''))
-          .subtitle(game_difficulty_labels[0].join('') + " - " + difficulty_7 + " | " + game_difficulty_labels[1].join('') + " - " + difficulty_8 + " | " + game_difficulty_labels[2].join('') + " - " + difficulty_9)
+          .title(game_names[2])
+          .subtitle(game_difficulty_labels[0] + " - " + difficulty_7 + " | " + game_difficulty_labels[1] + " - " + difficulty_8 + " | " + game_difficulty_labels[2] + " - " + difficulty_9)
           .icon(image_path_3)
-          .arg(game_id[2].join(''))
+          .arg(game_id[2])
   end
 else
   workflow.result
@@ -129,4 +136,4 @@ else
         .subtitle("Example: Doom")
         .arg(query)
 end
-print workflow.output
+puts workflow.output
